1cd76ed0a930d05e774a0d434a510c9166676209
DaniaCerpa <dania.cerpa@gmail.com>
2024-08-11 23:35:54 -0400

    Primer commit

---
+++ tercer.py
@@ -0,0 +1,19 @@
+#Creacion de variables
+a = 8
+b = 10.5
+c = "ejercicio"
+
+miconjunto = {a, b, c}
+
+#Creacion de lista que contenga el conjunto 
+milista = [miconjunto, False]
+print ("milista :",milista)
+
+"""
+#Otra forma de pasar de set a lista y agregarle un nuevo dato (ejemplo)
+miconjunto1 =list(miconjunto)
+print (miconjunto1)
+
+miconjunto1.append(False)
+print (miconjunto1)
+"""
